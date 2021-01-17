2984a10b1a07a31978a48c5fdef437df0141d0c9
ntn <neethan.bala@gmail.com>
2021-01-17 09:17:26 -0500

    Update with latest prototype

--- background.js
+++ background.js
@@ -2,14 +2,28 @@
 var pageConditions = {
   conditions: [
     new chrome.declarativeContent.PageStateMatcher({
-      pageUrl: { schemes: ['https','http'] }
+      pageUrl: { schemes: ['https', 'http'] }
     })
   ],
   actions: [new chrome.declarativeContent.ShowPageAction()]
 }
 
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
     chrome.declarativeContent.onPageChanged.addRules([pageConditions]);
   });
+});
+
+var products;
+
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  if (message.method == "getCart") sendResponse(products);
+});
+
+chrome.runtime.onMessageExternal.addListener(function (
+  message,
+  sender,
+  sendResponse
+) {
+  if (message.method == "setCart") products = message;
 });
--- popup/popup.js
+++ popup/popup.js
@@ -23,5 +23,3 @@ chrome.tabs.query({
   });
 });
 
-
-
